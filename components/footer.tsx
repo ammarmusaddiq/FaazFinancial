9120f4feb1b90e94afa7bd0a62c523d0b35bd15d
ammar <ammarmu007@gmail.com>
2025-09-12 21:35:15 +0500

    footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -146,7 +146,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 FaazFinancialGroup. All rights reserved.</p>
+          <p> Copyright 2025 © FTSSolution.tech All Right Reserved.</p>
         </div>
       </div>
     </footer>
